39912de72fd507c9bd772aadb0d0593797c969f0
wieb0028 <wieb0028@algonquinlive.com>
2023-05-19 13:33:35 -0400

    added examples and arrays

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("js file linked to html")
+// console.log("js file linked to html")
+/*
+Line 1 is a single line comment 
+but this comment is a multiline one
+*/
+/* Shift+alt+a creates a multiline comment over higlighted text */
+
+
+let myName = "Cameron"
+console.log(myName)
+
+myName = "Cameron Wiebe"
+console.log(myName)
+
+const address = "earth"
+console.log(address)
+
+let myProfession
+myProfession = "worker"
+console.log(myProfession)
+
+/* An array */
+const myScore = [56, 76, 98]
+console.log(myScore)
